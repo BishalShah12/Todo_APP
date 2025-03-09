ea3fc163bd18be874d639011a4906fc0fbde21a8
Bishal <bishalshah444999@gmail.com>
2025-03-09 17:44:40 +0545

    try

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,10 +4,7 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({
-    origin: 'https://todo-app-i3dc.vercel.app',
-    credentials: true, 
-}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("/public"))
