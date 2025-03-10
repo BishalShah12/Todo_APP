48fb20f65846cd48480d51c8d2ce39cabf151510
Bishal <bishalshah444999@gmail.com>
2025-03-11 00:42:30 +0545

    went wrong

--- Frontend/test/src/components/pages/Header/Header.jsx
+++ Frontend/test/src/components/pages/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {useSelector, useDispatch} from "react-redux"
-import { logoutThunk } from '../../store/slice/user/user.thunk';
+import { logoutThunk } from '../../store/slice/user/user.thunk.js';
 import LoginPage from '../../User/LogInPage.jsx';
 
 const Header = () => {
