52c36917a7a82cf17c251ec7d85297e8fd698cd7
Bishal <bishalshah444999@gmail.com>
2025-03-11 00:32:18 +0545

    llllll

--- Frontend/src/components/utils/axiosInstance.js
+++ Frontend/src/components/utils/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
-const DB_URL = import.meta.env.VITE_DB_URL
+// const DB_URL = import.meta.env.VITE_DB_URL
 // const DB_URL = "https://todo-app-theta-jet-14.vercel.app/api/v1"
 export const axiosInstance = axios.create({
-    baseURL: DB_URL,
     withCredentials:true,
+    baseURL: "https://todo-app-theta-jet-14.vercel.app/api/v1",
     headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer token' // If applicable
