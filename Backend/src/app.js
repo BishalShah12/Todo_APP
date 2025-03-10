768cbc275f1f165a91e9f7a36a725d9d6596a230
Bishal <bishalshah444999@gmail.com>
2025-03-10 18:40:08 +0545

    nnnnn

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: ['http://localhost:5173','https://voluble-bublanina-c048f9.netlify.app/'], 
+        origin: ['http://localhost:5173','https://voluble-bublanina-c048f9.netlify.app'], 
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true
